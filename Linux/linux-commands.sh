7521601526fabde639f6923fbc823e13bbc81449
Andile <andile.ntumba@sambeconsulting.com>
2025-04-02 10:41:00 +0200

    Added linux file

---
+++ Linux/linux-commands.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+$ pwd /home/ndiletumba # Displays the present working directory name
+
+$ ls  #  Lists directory contents
+
+$ cd  # Changes the current directory
+
+$ cat #  Concatenates and displays files
+
+$  echo # Displays arguments to the screen
+
+$  clear # Clears the screen
+
+$  exit # Exit the current session
+
+$ ls -l #  Lists directory contents using a 
+#long listing format.
+
+$ rmdir testumask # removes a directory
+
+$ ls -latr # Long listing including all files 
+#reverse sorted by time.
+
+$ tree -d # list directories only
+
+chmod 755 <file> # Change file permissions
+
+sudo # Execute a command as the superuser
+
+df -h # Show disk usage
+
+
+
